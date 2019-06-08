bc124fc69ec3cc695727f31e3f123324f9b66897
Shivesh Dewangan <shivesh.dewangan@gmail.com>
2019-06-08 15:18:09 +1000

    firebase config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC4hYj9_q-H5K25lryPC6YHKkm8iJusgwI",
+  authDomain: "hooks-news-app-a3cab.firebaseapp.com",
+  databaseURL: "https://hooks-news-app-a3cab.firebaseio.com",
+  projectId: "hooks-news-app-a3cab",
+  storageBucket: "hooks-news-app-a3cab.appspot.com",
+  messagingSenderId: "971482448179",
+  appId: "1:971482448179:web:7db6eb31e634c431"
+};
+
+export default firebaseConfig;
